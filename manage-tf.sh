adce74ae1f69405c663c8ac9d5c7a73faf037353
BOlimpio <b.olimpio@outlook.com>
2024-03-20 23:22:17 -0300

    add new features to be validate - import, find and output

--- manage-tf.sh
+++ manage-tf.sh
@@ -1,78 +1,73 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
-    then
-        echo "Selecione uma das opcoes"
-        echo "Para config inicial rodar um 'source deploy.sh config'
-        echo $0" <config | fmt | init | plan | apply | destroy | clean | go | goodbye>" 
-        exit 1
+if [ $# -eq 0 ]; then
+    echo "Select one of the options"
+    echo "To initialize config, run 'source deploy.sh config'"
+    echo "$0 <config | fmt | init | plan | apply | destroy | clean | go | goodbye | import | find | output>"
+    exit 1
 fi 
-
-#implementar um ambiente pos config pra não precisar entrar no script e trocar o ENV toda vez
-
 
 case $1 in
     config)
-    export TF_LOG=debug                                             # modo DEBUG
-    export TF_LOG_PATH=./terraform.log                              # cria arquivo LOG
-    export TF_PLUGIN_CACHE_DIR="$APPDATA/terraform.d/plugin-cache"  # fornece um diretorio para cache dos plugins
-    export TF_VAR_repo=`echo "${PWD##*/}"`                          # configura nome do repositório de origem
-    export ENV="your_env"
-    
-    ;; 
+        export TF_LOG=debug                                             # DEBUG mode
+        export TF_LOG_PATH=./terraform.log                              # creates a LOG file
+        export TF_PLUGIN_CACHE_DIR="$APPDATA/terraform.d/plugin-cache"  # provides a directory for plugin cache
+        export TF_VAR_repo=`echo "${PWD##*/}"`                          # sets the repository name
+        export ENV="your_env"
+        ;; 
     fmt)
-        echo "## Iniciando $1. ##"
+        echo "## Starting $1. ##"
         terraform fmt
         terraform fmt tfvars/
         terraform validate
-        duration=$SECONDS # armazena a duração do processo na variável 'duration'
-        echo "Levou $duration segundos para executar o $1."
+        duration=$SECONDS # stores the duration of the process in the 'duration' variable
+        echo "Took $duration seconds to execute $1."
         ;; 
     init)
         SECONDS=0
-        echo "## Iniciando $1. ##"
+        echo "## Starting $1. ##"
         terraform init
-        duration=$SECONDS # armazena a duração do processo na variável 'duration'
-        echo "Levou $duration segundos para executar o $1."
+        duration=$SECONDS # stores the duration of the process in the 'duration' variable
+        echo "Took $duration seconds to execute $1."
         ;;
     plan)
         SECONDS=0
-        echo "## Iniciando $1. ##"
+        echo "## Starting $1. ##"
         terraform fmt
         terraform validate
         terraform plan -var-file="tfvars/$ENV.tfvars" -out="$ENV.tfplan"
-        duration=$SECONDS # armazena a duração do processo na variável 'duration'
-        echo "## Levou $duration segundos para executar o $1. ##"
+        duration=$SECONDS # stores the duration of the process in the 'duration' variable
+        echo "## Took $duration seconds to execute $1. ##"
         ;;
     apply)
         SECONDS=0
-        echo "## Iniciando $1. ##"
+        echo "## Starting $1. ##"
         terraform apply -auto-approve "$ENV.tfplan"
-        duration=$SECONDS # armazena a duração do processo na variável 'duration'
-        echo "Levou $duration segundos para executar o $1."
+        duration=$SECONDS # stores the duration of the process in the 'duration' variable
+        echo "Took $duration seconds to execute $1."
         ;;
     go)
         SECONDS=0
-        echo "## Iniciando $1. ##"
+        echo "## Starting $1. ##"
         terraform plan -var-file="tfvars/$ENV.tfvars" -out="$ENV.tfplan"
         terraform apply -auto-approve "$ENV.tfplan"
-        duration=$SECONDS # armazena a duração do processo na variável 'duration'
-        echo "Levou $duration segundos para executar o $1."
+        duration=$SECONDS # stores the duration of the process in the 'duration' variable
+        echo "Took $duration seconds to execute $1."
         ;;
     goodbye)
         SECONDS=0
-        echo "## Iniciando $1. ##"
+        echo "## Starting $1. ##"
         terraform plan -var-file="tfvars/$ENV.tfvars" -out="$ENV.tfplan"
         terraform destroy -auto-approve "$ENV.tfplan"
-        duration=$SECONDS # armazena a duração do processo na variável 'duration'
-        echo "Levou $duration segundos para executar o $1."
+        duration=$SECONDS # stores the duration of the process in the 'duration' variable
+        echo "Took $duration seconds to execute $1."
         ;;
     destroy)
         SECONDS=0
-        echo "## Executando $1. ##"
+        echo "## Executing $1. ##"
         terraform destroy -auto-approve -var-file="tfvars/$ENV.tfvars"
-        duration=$SECONDS # armazena a duração do processo na variável 'duration'
-        echo "## Levou $duration segundos para executar o $1."
+        duration=$SECONDS # stores the duration of the process in the 'duration' variable
+        echo "## Took $duration seconds to execute $1."
         ;;
     clean)
         rm -rf .terraform
@@ -82,13 +77,39 @@ case $1 in
         rm -rf *.tfstate
         rm -rf *.backup
         rm -rf .terraform.lock.hcl
-        echo "$1 finalizado."
+        echo "$1 completed."
         ;;
     lock)
         terraform providers lock
-        echo "$1 finalizado."
+        echo "$1 completed."
+        ;;
+    import)
+        echo "## Starting $1. ##"
+        # Asks the user for the resource type and ID to import
+        read -p "Enter the resource type and ID to import (in the format <resource_type>.<resource_name> or -id=<resource_id>): " terraform_resource
+        # Executes the 'terraform import' command with the provided resource and ID
+        terraform import $terraform_resource
+        ;;
+    find)
+        echo "## Starting $1. ##"
+        # Asks the user for the resource to find
+        read -p "Enter the resource to find (in the format <resource_type>.<resource_name> or -id=<resource_id>): " resource_identifier
+        # Executes the 'terraform state list' command with the provided resource identifier
+        terraform state list $resource_identifier
+        ;;
+    output)
+        echo "## Starting $1. ##"
+        # Asks the user for the output name to retrieve, or leave blank to retrieve all outputs
+        read -p "Enter the output name to retrieve (leave blank for all outputs): " output_name
+        if [ -z "$output_name" ]; then
+            # If no output name is provided, list all outputs
+            terraform output
+        else
+            # If an output name is provided, show the specified output
+            terraform output $output_name
+        fi
         ;;
     *)
-        echo "erro, selecione uma opcao valida."
+        echo "Error: Select a valid option."
         ;;
 esac
